df38affb00dad65f7085bdaa3fc5de7d158049c8
raihara3 <raihara3@gmail.com>
2021-03-28 23:50:57 +0900

    :package: add @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   assetPrefix: process.env.ASSET_PREFIX,
   poweredByHeader: false,
   reactStrictMode: true,
-}
+})
