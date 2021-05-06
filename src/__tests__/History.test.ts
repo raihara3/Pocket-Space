796113328344b9a37a98e8a30998f050e115cbdb
raihara3 <raihara3@gmail.com>
2021-05-06 12:50:53 +0900

    add History.test.ts

---
+++ src/__tests__/History.test.ts
@@ -0,0 +1,23 @@
+import History from '../History'
+
+describe('History', () => {
+  const SAVE_LENGTH = 5
+  const history = new History(SAVE_LENGTH)
+
+  test('add', () => {
+    history.add('his1')
+    history.add('his2')
+    history.add('his3')
+    expect(history.getHistory).toEqual([null, null, 'his1', 'his2', 'his3'])
+  })
+
+  test('back', () => {
+    history.back()
+    expect(history.getHistory).toEqual([null, null, null, 'his1', 'his2'])
+  })
+
+  test('deleteAll', () => {
+    history.deleteAll()
+    expect(history.getHistory).toEqual([null, null, null, null, null])
+  })
+})
